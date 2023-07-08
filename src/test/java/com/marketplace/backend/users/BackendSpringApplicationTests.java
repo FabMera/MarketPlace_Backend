86e50d948e695ee4dea9f482e8c9fffd3a9357f2
FabMera <104039918+FabMera@users.noreply.github.com>
2023-07-08 13:09:24 -0400

    backend users marketplace react

---
+++ src/test/java/com/marketplace/backend/users/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.backend.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
